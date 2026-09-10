38be9bb84aa371c79cdcf51529ecebfd9d887fff
pishivato <91039813+pishivato@users.noreply.github.com>
2026-09-10 12:31:34 -0700

    feat: ejercicios semana 8-9 JS moderno

--- semana8/ejercicio3.js
+++ semana8/ejercicio3.js
@@ -1,10 +1,19 @@
 const obtenerEquipos = async () => {
     const respuesta = await fetch("http://127.0.0.1:8000/equipos");
     const datos = await respuesta.json();
-    for (const item of datos)
+    const primero = datos[0];
+    const primeroActualizar = {...primero, ip : "172.16.0.99"};
+
+    console.log(primero);
+    console.log(primeroActualizar);
+
+    for (const {id, nombre, tipo, ip, ubicacion, activo} of datos)
     {
-        console.log(`ID: ${item.id}, Nombre: ${item.nombre}, Tipo: ${item.tipo}, Ubicacion: ${item.ubicacion}, Estado: ${item.activo ? "✅":"❌"}`);
+        console.log(`ID: ${id}, Nombre: ${nombre}, Tipo: ${tipo}, IP: ${ip}, Ubicacion: ${ubicacion}, Estado: ${activo ? "✅":"❌"}`);
     }
 }
 
 obtenerEquipos();
+
+
+
